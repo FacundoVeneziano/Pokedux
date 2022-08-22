d26756a41338eb7cf1788c0b2cc6288974611fc3
FacundoVeneziano <facundo.e.veneziano@gmail.com>
2022-08-22 01:59:00 -0300

    add favorite

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,26 @@
-import { StarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import Meta from "antd/lib/card/Meta";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setFavorite } from "../actions";
 import "./PokemonList.css";
+import StarButton from "./StarButton";
 
-const PokemonCard = ({ name, image }) => {
+const PokemonCard = ({ name, image, types, id, favorite }) => {
+  const dispatch = useDispatch();
+  const typePokemon = types.map((e) => e.type.name).join(", ");
+
+  const handleOnFavorite = () => {
+    dispatch(setFavorite({pokemonId: id}));
+  };
+
   return (
     <Card
       title={name}
       cover={<img src={image} alt={name} />}
-      extra={<StarOutlined />}
+      extra={<StarButton isFavorite={favorite} onClick={handleOnFavorite} />}
     >
-      <Meta description="fire, magic" />
+      <Meta description={typePokemon} />
     </Card>
   );
 };
--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,4 +1,4 @@
-import { SET_LOADING, SET_POKEMONS } from "../actions/types";
+import { SET_FAVORITE, SET_LOADING, SET_POKEMONS } from "../actions/types";
 
 const initialState = {
   pokemons: [],
@@ -12,6 +12,17 @@ export const pokemonsReducer = (state = initialState, action) => {
         ...state,
         pokemons: action.payload,
       };
+    case SET_FAVORITE:
+      const newPokemonsList = [...state.pokemons];
+      const currentPokemonIndex = newPokemonsList.findIndex((pokemon) => {
+        return pokemon.id === action.payload.pokemonId;
+      });
+      if (currentPokemonIndex < 0) {
+        return state;
+      }
+      newPokemonsList[currentPokemonIndex].favorite =
+        !newPokemonsList[currentPokemonIndex].favorite;
+      return { ...state, pokemons: newPokemonsList };
     case SET_LOADING:
       return {
         ...state,
